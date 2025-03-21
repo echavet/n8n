3ad877799dc5e6bb9d9113391b525edd64ad0947
Eric Chavet <echavet@gmail.com>
2025-03-21 11:43:17 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/with-contenv bashio
-# tail -f /dev/null
-# exec /usr/bin/n8n start
 
+WEBHOOK_URL=$(bashio::config 'webhook_url')
 export N8N_HOST=0.0.0.0
 export N8N_PORT=5678
-export WEBHOOK_URL=https://n8n.pool-io.com
+export WEBHOOK_URL="$WEBHOOK_URL"
 export N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS=true
 export N8N_CONFIG_DIR=/data
 
-npx n8n start
+# Lancer n8n
+n8n start
+#npx n8n start
