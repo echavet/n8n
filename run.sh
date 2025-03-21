08a63567a7c55c211522afabcc324b560123a5b2
Eric Chavet <echavet@gmail.com>
2025-03-21 11:29:44 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@
 # tail -f /dev/null
 # exec /usr/bin/n8n start
 export N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS=true
-npx n8n start --host=0.0.0.0 --webhook-url=https://n8n.pool-io.com -c /data
+export N8N_HOST=0.0.0.0
+export N8N_PORT=5678
+export WEBHOOK_URL=https://n8n.pool-io.com
+npx n8n start
